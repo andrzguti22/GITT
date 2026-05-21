b5ef1908c031da22495604fafde5581463640b58
andrzguti22 <andrzgut@gmail.com>
2026-05-21 15:51:03 -0500

    feat: agrego saludo.py

---
+++ saludo.py
@@ -0,0 +1,10 @@
+# saludo.py
+# Completá con tus datos reales
+
+nombre  = "Andrés Gutierrez"
+carrera = "Análisis y Desarrollo de Software"
+proyecto = "MicroJobs"
+
+print(f"Hola, soy {nombre}")
+print(f"Estudio {carrera} en el SENA")
+print(f"Mi proyecto es: {proyecto}")
